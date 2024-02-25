076ddedaa9b75a3743ce0fc6841dbedc5a17b3ae
hujianzhe <976784480@qq.com>
2024-02-26 02:18:53 +0800

    string

--- common/string.js
+++ common/string.js
@@ -1,27 +1,27 @@
 var js_util = js_util || {};
 js_util.Common = js_util.Common || {};
 
-js_util.Common.string_to_utf8 = function(str) {
-	let utf8 = "";
+js_util.Common.string_to_utf8_bytes = function(str) {
+	let utf8 = [];
 	for (let i = 0; i < str.length; i++) {
 		let code = str.charCodeAt(i);
 		if (code < 0x0080) {
-			utf8 += str.charAt(i);
+			utf8.push(code);
 		}
 		else if (code < 0x0800) {
-			utf8 += String.fromCharCode(0xC0 | ((code >> 6) & 0x1F));
-			utf8 += String.fromCharCode(0x80 | (code & 0x3F));
+			utf8.push(0xC0 | ((code >> 6) & 0x1F));
+			utf8.push(0x80 | (code & 0x3F));
 		}
 		else if (code < 0x10000) {
-			utf8 += String.fromCharCode(0xE0 | ((code >> 12) & 0x0F));
-			utf8 += String.fromCharCode(0x80 | ((code >>  6) & 0x3F));
-			utf8 += String.fromCharCode(0x80 | (code & 0x3F));
+			utf8.push(0xE0 | ((code >> 12) & 0x0F));
+			utf8.push(0x80 | ((code >>  6) & 0x3F));
+			utf8.push(0x80 | (code & 0x3F));
 		}
 		else if (code < 0x110000) {
-			utf8 += String.fromCharCode(0xF0 | ((code >> 18) & 0x07));
-			utf8 += String.fromCharCode(0x80 | ((code >> 12) & 0x3F));
-			utf8 += String.fromCharCode(0x80 | ((code >> 6) & 0x3F));
-			utf8 += String.fromCharCode(0x80 | (code & 0x3F));
+			utf8.push(0xF0 | ((code >> 18) & 0x07));
+			utf8.push(0x80 | ((code >> 12) & 0x3F));
+			utf8.push(0x80 | ((code >> 6) & 0x3F));
+			utf8.push(0x80 | (code & 0x3F));
 		}
 		else {
 			throw "string isn't utf-16";
@@ -30,30 +30,29 @@ js_util.Common.string_to_utf8 = function(str) {
 	return utf8;
 };
 
-js_util.Common.utf8_to_string = function(utf8) {
+js_util.Common.utf8_bytes_to_string = function(utf8) {
 	let str = "";
-	for(let i = 0; i < utf8.length; i++) {
-		if(utf8.charCodeAt(i) >> 7 === 0x00) {
-			str += utf8.charAt(i);
+	for (let i = 0; i < utf8.length; i++) {
+		if (utf8[i] >> 7 === 0x00) {
+			str += String.fromCharCode(utf8[i]);
 		}
-		else if (utf8.charCodeAt(i) >> 5 === 0x06) {
-			let tmp = ((utf8.charCodeAt(i + 0) & 0x1f) << 6) |
-					((utf8.charCodeAt(i + 1) & 0x3f) << 0);
+		else if (utf8[i] >> 5 === 0x06) {
+			let tmp = ((utf8[i] & 0x1f) << 6) | (utf8[i + 1] & 0x3f);
 			str += String.fromCharCode(tmp);
 			i++;
 		}
-		else if(utf8.charCodeAt(i) >> 4 === 0x0e) {
-			let tmp = ((utf8.charCodeAt(i + 0) & 0x0f) << 12) |
-					((utf8.charCodeAt(i + 1) & 0x3f) <<  6) |
-					((utf8.charCodeAt(i + 2) & 0x3f) <<  0);
+		else if (utf8[i] >> 4 === 0x0e) {
+			let tmp = ((utf8[i] & 0x0f) << 12) |
+						((utf8[i + 1] & 0x3f) <<  6) |
+						(utf8[i + 2] & 0x3f);
 			str += String.fromCharCode(tmp);
 			i += 2;
 		}
-		else if(utf8.charCodeAt(i) >> 3 === 0x1f) {
-			let tmp = ((utf8.charCodeAt(i + 0) & 0x07) << 18) |
-					((utf8.charCodeAt(i + 1) & 0x3f) << 12) |
-					((utf8.charCodeAt(i + 2) & 0x3f) <<  6) |
-					((utf8.charCodeAt(i + 3) & 0x3f) <<  0);
+		else if (utf8[i] >> 3 === 0x1f) {
+			let tmp = ((utf8[i] & 0x07) << 18) |
+						((utf8[i + 1] & 0x3f) << 12) |
+						((utf8[i + 2] & 0x3f) <<  6) |
+						(utf8[i + 3] & 0x3f);
 			str += String.fromCharCode(tmp);
 			i += 3;
 		}
@@ -64,19 +63,19 @@ js_util.Common.utf8_to_string = function(utf8) {
 	return str;
 };
 
-js_util.Common.utf8_fill_bytes = function(utf8, dv, off) {
-	for (let i = 0; i < utf8.length; ++i) {
-		dv.setUint8(off + i, utf8.charCodeAt(i));
+js_util.Common.bytes_fill_buffer = function(bytes, dv, off) {
+	for (let i = 0; i < bytes.length; ++i) {
+		dv.setUint8(off + i, bytes[i]);
 	}
 };
 
-js_util.Common.bytes_to_utf8 = function(dv) {
-	let utf8 = "";
+js_util.Common.buffer_to_bytes = function(dv) {
+	let bytes = [];
 	const byteLength = dv.byteLength;
 	for (let i = 0; i < byteLength; ++i) {
-		utf8 += String.fromCharCode(dv.getUint8(i));
+		bytes.push(dv.getUint8(i));
 	}
-	return js_util.Common.utf8_to_string(utf8);
+	return bytes;
 };
 
 js_util.Common.string_trim = function(string) {

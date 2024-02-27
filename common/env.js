0a58a070ffeb87c26a9c89caa83ff1b89d098f8e
zzz <zzz@taomee-ex.com>
2024-02-27 11:41:21 +0800

    env

--- common/env.js
+++ common/env.js
@@ -7,6 +7,12 @@ js_util.Common.HOST_ENV = {
     NODE: 2
 };
 
+js_util.Common.HOST_BROWSER_ENV = {
+    UNKKNOW : 0,
+    PC : 1,
+    MOBILE : 2
+};
+
 js_util.Common.host_env = function() {
     return (new Function("\
     try {\
@@ -18,13 +24,35 @@ js_util.Common.host_env = function() {
     return js_util.Common.HOST_ENV.UNKKNOW;"))();
 };
 
-js_util.Common.host_env_name = function () {
+js_util.Common.host_env_string = function () {
     const env = js_util.Common.host_env();
     switch (env) {
         case js_util.Common.HOST_ENV.BROWSER:
             return "browser";
         case js_util.Common.HOST_ENV.NODE:
             return "node";
+    }
+    return "";
+};
+
+js_util.Common.host_browser_env = function () {
+    const env = js_util.Common.host_env();
+    if (env != js_util.Common.HOST_ENV.BROWSER) {
+        return js_util.Common.HOST_BROWSER_ENV.UNKKNOW;
+    }
+    if ('ontouchstart' in document) {
+        return js_util.Common.HOST_BROWSER_ENV.MOBILE;
+    }
+    return js_util.Common.HOST_BROWSER_ENV.PC;
+};
+
+js_util.Common.host_browser_env_string = function () {
+    const env = js_util.Common.host_browser_env();
+    switch (env) {
+        case js_util.Common.HOST_BROWSER_ENV.PC:
+            return "PC";
+        case js_util.Common.HOST_BROWSER_ENV.MOBILE:
+            return "mobile";
     }
     return "";
 };

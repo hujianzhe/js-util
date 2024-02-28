f69256f78558979299e508db17c4dfdd64d550b6
zzz <zzz@taomee-ex.com>
2024-02-28 11:47:45 +0800

    typeof

--- common/typeof.js
+++ common/typeof.js
@@ -5,6 +5,7 @@ js_util.Common = js_util.Common || {};
 
 js_util.Common.type_of = function (v) {
     // Object.prototype.toString.call(123); // "[object Number]"
+    // Object.prototype.toString.call(123n); // "[object BigInt]"
     // Object.prototype.toString.call("hello"); // "[object String]"
     // Object.prototype.toString.call(true); // "[object Boolean]"
     // Object.prototype.toString.call(undefined); // "[object Undefined]"
@@ -14,5 +15,12 @@ js_util.Common.type_of = function (v) {
     // Object.prototype.toString.call(function(){}); // "[object Function]"
     // Object.prototype.toString.call(new Date()); // "[object Date]"
     // Object.prototype.toString.call(new RegExp()); // "[object RegExp]"
-	return Object.prototype.toString.call(v).slice(8, -1);
+	const type_str = Object.prototype.toString.call(v).slice(8, -1);
+    if ("Object" != type_str) {
+        return type_str;
+    }
+    if (v.constructor.name) {
+        return v.constructor.name;
+    }
+    return type_str;
 };

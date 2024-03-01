4af4f06800c24a15dc4dc9eb21eff06613ea4177
zzz <zzz@taomee-ex.com>
2024-03-01 16:17:29 +0800

    lock and keyboard

---
+++ DOM/keyboard.js
@@ -0,0 +1,63 @@
+if (typeof js_util === 'undefined') {
+    js_util = {};
+}
+js_util.DOM = js_util.DOM || {};
+
+if (!js_util.DOM.KeyboardListener) {
+    class KeyboardKeyState {
+        constructor() {
+            this.pressed = false;
+            this.press_timestamp = 0;
+            this.press_start_timestamp = 0;
+            this.release_timestamp = 0;
+            this.press_total_times = 0;
+        }
+    }
+    class KeyboardListener {
+        constructor() {
+            this.key_states_map = new Map();
+        }
+
+        key_state(code) {
+            return this.key_states_map.get(code) || new KeyboardKeyState();
+        }
+    }
+    js_util.DOM.KeyboardListener = new KeyboardListener();
+
+    document.addEventListener('keydown', function (e) {
+		if ('Unidentified' === e.code || '' === e.code) {
+            return;
+        }
+        let state = js_util.DOM.KeyboardListener.key_states_map.get(e.code);
+        if (!state) {
+            state = new KeyboardKeyState();
+            js_util.DOM.KeyboardListener.key_states_map.set(e.code, state);
+        }
+        const now = Date.now();
+        if (!state.pressed) {
+            state.pressed = true;
+            ++state.press_total_times;
+            state.press_start_timestamp = now;
+        }
+        state.press_timestamp = now;
+	}, true);
+
+    document.addEventListener('keyup', function (e) {
+        if ('Unidentified' === e.code || '' === e.code) {
+            return;
+        }
+        let state = js_util.DOM.KeyboardListener.key_states_map.get(e.code);
+        if (!state) {
+            state = new KeyboardKeyState();
+            js_util.DOM.KeyboardListener.key_states_map.set(e.code, state);
+        }
+        const now = Date.now();
+        state.pressed = false;
+        state.release_timestamp = now;
+        state.press_timestamp = now;
+        if (state.press_start_timestamp <= 0) {
+            state.press_start_timestamp = now;
+            state.press_total_times = 1;
+        }
+    }, true);
+};
--- common/lock.js
+++ common/lock.js
@@ -3,138 +3,142 @@ if (typeof js_util === 'undefined') {
 }
 js_util.Common = js_util.Common || {};
 
-js_util.Common.LockImpl = class LockImpl {
-    constructor(name) {
-        this.owner = null;
-        this.enter_times = 0;
-        this.waitArr = [];
-        this.name = name;
-    }
+if (!js_util.Common.LockManager) {
+    js_util.Common.LockManager = class LockManager {
+        constructor() {
+            this.lockMap = new Map();
+        }
 
-    try_acquire(owner) {
-        if (!this.owner) {
-            this.owner = owner;
-            this.enter_times = 1;
-            return true;
-        }
-        else if (owner == this.owner) {
-            this.enter_times++;
-            return true;
-        }
-        return false;
-    }
-
-    acquire(owner) {
-        if (this.try_acquire(owner)) {
-            return;
-        }
-        let self = this;
-        return new Promise((resolve) => {
-            self.waitArr.push({
-                resolve: resolve,
-                owner: owner
-            });
-        });
-    }
-
-    release() {
-        if (this.enter_times > 1) {
-            this.enter_times--;
-            return;
-        }
-        if (this.waitArr.length <= 0) {
-            this.owner = null;
-            return;
-        }
-        const obj = this.waitArr.shift();
-        this.owner = obj.owner;
-        obj.resolve();
-        let i;
-        for (i = 0; i < this.waitArr.length; ++i) {
-            if (this.waitArr[i].owner != this.owner) {
-                break;
+        new_guard(owner) {
+            if (!owner) {
+                owner = new Object();
             }
-            this.enter_times++;
-            this.waitArr[i].resolve();
+            return new LockGuard(this, owner);
         }
-        if (i <= 0) {
-            return;
-        }
-        if (i >= this.waitArr.length) {
-            this.waitArr.length = 0;
-            return;
-        }
-        this.waitArr.splice(0, i);
-    }
-};
 
-js_util.Common.LockGuard = class LockGuard {
-    constructor(mgr, owner) {
-        this.owner = owner;
-        this.mgr = mgr;
-        this.lk = null;
-    }
-
-    try_lock(name) {
-        if (this.lk) {
-            throw new Error(`lock has locked, name: ${this.lk.name}`);
+        static unlock_guards(guardArr) {
+            if (!guardArr) {
+                return;
+            }
+            for (let i = guardArr.length; i > 0; --i) {
+                guardArr[i - 1].unlock();
+            }
         }
-        let lk = this.mgr._private_get_or_new_lock(name);
-        if (!lk.try_acquire(this.owner)) {
+    };
+
+    // Implement Hide
+
+    class LockImpl {
+        constructor(name) {
+            this.owner = null;
+            this.enter_times = 0;
+            this.waitArr = [];
+            this.name = name;
+        }
+
+        try_acquire(owner) {
+            if (!this.owner) {
+                this.owner = owner;
+                this.enter_times = 1;
+                return true;
+            }
+            else if (owner == this.owner) {
+                this.enter_times++;
+                return true;
+            }
             return false;
         }
-        this.lk = lk;
-        return true;
+
+        acquire(owner) {
+            if (this.try_acquire(owner)) {
+                return;
+            }
+            let self = this;
+            return new Promise((resolve) => {
+                self.waitArr.push({
+                    resolve: resolve,
+                    owner: owner
+                });
+            });
+        }
+
+        release() {
+            if (this.enter_times > 1) {
+                this.enter_times--;
+                return;
+            }
+            if (this.waitArr.length <= 0) {
+                this.owner = null;
+                return;
+            }
+            const obj = this.waitArr.shift();
+            this.owner = obj.owner;
+            obj.resolve();
+            let i;
+            for (i = 0; i < this.waitArr.length; ++i) {
+                if (this.waitArr[i].owner != this.owner) {
+                    break;
+                }
+                this.enter_times++;
+                this.waitArr[i].resolve();
+            }
+            if (i <= 0) {
+                return;
+            }
+            if (i >= this.waitArr.length) {
+                this.waitArr.length = 0;
+                return;
+            }
+            this.waitArr.splice(0, i);
+        }
     }
 
-    async lock(name) {
-        if (this.lk) {
-            throw new Error(`lock has locked, name: ${this.lk.name}`);
-        }
-        let lk = this.mgr._private_get_or_new_lock(name);
-        await lk.acquire(this.owner);
-        this.lk = lk;
-    }
-
-    unlock() {
-        if (!this.lk) {
-            return;
-        }
-        this.lk.release();
-        if (!this.lk.owner) {
-            this.mgr.lockMap.delete(this.lk.name);
-        }
-        this.lk = null;
-    }
-
-    static unlock_guards(guardArr) {
-        if (!guardArr) {
-            return;
-        }
-        for (let i = guardArr.length; i > 0; --i) {
-            guardArr[i - 1].unlock();
-        }
-    }
-};
-
-js_util.Common.LockManager = class LockManager {
-    constructor() {
-        this.lockMap = new Map();
-    }
-
-    _private_get_or_new_lock(name) {
-        let lock = this.lockMap.get(name);
+    function lockmgr_private_get_or_new_lock(mgr, name) {
+        let lock = mgr.lockMap.get(name);
         if (!lock) {
-            lock = new js_util.Common.LockImpl(name);
-            this.lockMap.set(name, lock);
+            lock = new LockImpl(name);
+            mgr.lockMap.set(name, lock);
         }
         return lock;
     }
 
-    new_guard(owner) {
-        if (!owner) {
-            owner = new Object();
+    class LockGuard {
+        constructor(mgr, owner) {
+            this.owner = owner;
+            this.mgr = mgr;
+            this.lk = null;
         }
-        return new js_util.Common.LockGuard(this, owner);
+
+        try_lock(name) {
+            if (this.lk) {
+                throw new Error(`lock has locked, name: ${this.lk.name}`);
+            }
+            let lk = lockmgr_private_get_or_new_lock(this.mgr, name);
+            if (!lk.try_acquire(this.owner)) {
+                return false;
+            }
+            this.lk = lk;
+            return true;
+        }
+
+        async lock(name) {
+            if (this.lk) {
+                throw new Error(`lock has locked, name: ${this.lk.name}`);
+            }
+            let lk = lockmgr_private_get_or_new_lock(this.mgr, name);
+            await lk.acquire(this.owner);
+            this.lk = lk;
+        }
+
+        unlock() {
+            if (!this.lk) {
+                return;
+            }
+            this.lk.release();
+            if (!this.lk.owner) {
+                this.mgr.lockMap.delete(this.lk.name);
+            }
+            this.lk = null;
+        }
     }
-};
+}

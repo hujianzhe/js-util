1962c0c20ece9e4fba0173acdacaf2f58bf5a483
hujianzhe <976784480@qq.com>
2024-05-14 23:26:08 +0800

    touch

--- DOM/touch.js
+++ DOM/touch.js
@@ -38,7 +38,7 @@ if (!js_util.DOM.TouchListener) {
         }
 
         touchmove(e) {
-            for (const touch of e.touches) {
+            for (const touch of e.changedTouches) {
                 let state = this.key_states_map.get(touch.identifier);
                 if (!state) {
                     state = new TouchState(touch.identifier);
@@ -59,7 +59,7 @@ if (!js_util.DOM.TouchListener) {
         }
 
         touchstart(e) {
-            for (const touch of e.touches) {
+            for (const touch of e.changedTouches) {
                 let state = this.key_states_map.get(touch.identifier);
                 if (!state) {
                     state = new TouchState(touch.identifier);

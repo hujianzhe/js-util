526db3f94f713435b7f81c4631e163acdfdded68
zzz <zzz@taomee-ex.com>
2024-05-24 13:13:27 +0800

    timeoutSet

--- common/timeout.js
+++ common/timeout.js
@@ -33,3 +33,59 @@ js_util.Common.clear_timeout_ex = function(ev) {
         ev._timerid = null;
     }
 };
+
+js_util.Common.TimeoutEventSet = class TimeoutSet {
+    constructor() {
+        this.timeout_set = new Set();
+        this.timeout_map = new Map();
+    }
+
+    add_timeout(fn, msec, special_id) {
+        let self = this;
+        if (special_id !== undefined) {
+            if (self.timeout_map.has(special_id)) {
+                return null;
+            }
+        }
+        let ev = js_util.Common.set_timeout_ex(function () {
+            self.clear_timeout(ev);
+            fn();
+        }, msec);
+        if (special_id !== undefined) {
+            ev.special_id = special_id;
+        }
+        else {
+            self.timeout_set.add(ev);
+        }
+        return ev;
+    }
+
+    remove_timeout(ev) {
+        js_util.Common.clear_timeout_ex(ev);
+        if (ev.special_id !== undefined) {
+            self.timeout_map.delete(ev.special_id);
+        }
+        else {
+            self.timeout_set.delete(ev);
+        }
+    }
+
+    remove_timeout_by_id(special_id) {
+        let ev = this.timeout_map.get(special_id);
+        if (!ev) {
+            return;
+        }
+        this.clear_timeout(ev);
+    }
+
+    remove_all() {
+        for (let ev of this.timeout_set) {
+            js_util.Common.clear_timeout_ex(ev);
+        }
+        this.timeout_set.clear();
+        for (let ev of this.timeout_map.values()) {
+            js_util.Common.clear_timeout_ex(ev);
+        }
+        this.timeout_map.clear();
+    }
+};

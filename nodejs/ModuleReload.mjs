bc3b1cc6bdf797ffe2c187da9f0096feaf01fcdb
zzz <zzz@taomee-ex.com>
2024-10-12 17:50:01 +0800

    module reload

---
+++ nodejs/ModuleReload.mjs
@@ -0,0 +1,24 @@
+import module from 'internal/process/esm_loader'; // --expose-internals
+import path from 'path';
+import url from 'url';
+
+class ModuleReload {
+    constructor(module_path) {
+        this.url = url.pathToFileURL(path.resolve(module_path)).href;
+        this.path = module_path;
+    }
+
+    async reload() {
+        if (module.esmLoader) {
+            module.esmLoader.moduleMap.delete(this.url);
+        }
+        else {
+            module.ESMLoader.moduleMap.delete(this.url);
+        }
+        await import(this.path);
+    }
+}
+
+module.exports = {
+    ModuleReload
+};

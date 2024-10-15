ac0e45305efd45f7b671919330b84e349f592d36
hujianzhe <976784480@qq.com>
2024-10-16 00:27:04 +0800

    refactor net channel

--- nodejs/net_channel_base.js
+++ nodejs/net_channel_base.js
@@ -2,10 +2,6 @@ const net = require('net');
 
 class NetChannelPipelineBase {
 	constructor() {
-		this.fnIoWrite = null;
-		this.fnIoFin = null;
-		this.fnIoDestroy = null;
-
 		this.fnDecodeBuffer = function (buff, rinfo) {
 			void buff, rinfo;
 			return null;
@@ -22,8 +18,6 @@ class NetChannelPipelineBase {
 class NetChannelBase {
 	static CLIENT_SIDE = 1;
 	static SERVER_SIDE = 2;
-	static PUBLISH_SIDE = 3;
-	static SUBSCRIBE_SIDE = 4;
 
 	static CONNECT_STATUS_NEW = 0;
 	static CONNECT_STATUS_DOING = 1;
@@ -53,18 +47,18 @@ class NetChannelBase {
 	}
 
 	constructor(pipeline, io, side, socktype) {
+		this._pipeline = pipeline;
 		this._io = io;
+		this._side = side;
 		this._socktype = socktype;
-		this._rbf = Buffer.alloc(0);
+
+		this._connectResolve = null;
+		this._connectStatus = NetChannelBase.CONNECT_STATUS_NEW;
+		this._connectTimeout = null;
 		this._heartbeatTimeout = null;
 		this._heartbeatTimes = 0;
 		this._lastRecvMsec = 0;
-		this._pipeline = pipeline;
-		this._side = side;
-		this._connectStatus = NetChannelBase.CONNECT_STATUS_NEW;
-		this._connectResolve = null;
-		this._ready_fin = false;
-		this._waitSendBufferWhenConnecting = null;
+
 		this.connectTimeoutMsec = 5000;
 		this.session = null;
 	}
@@ -73,183 +67,48 @@ class NetChannelBase {
 		return this._side;
 	}
 
-	_onClose(err) {
-		if (this._io) {
-			if (this._pipeline) {
-				this._pipeline.fnIoDestroy(this._io);
-			}
-			this._io = null;
+	get socktype() {
+		return this._socktype;
+	}
+
+	close(err) {
+		this._io = null;
+		if (this._connectTimeout) {
+			clearTimeout(this._connectTimeout);
+			this._connectTimeout = null;
+		}
+		if (this._connectResolve) {
+			this._connectResolve();
+			this._connectResolve = null;
 		}
 		if (this._heartbeatTimeout) {
 			clearTimeout(this._heartbeatTimeout);
 			this._heartbeatTimeout = null;
 		}
-		if (this._connectResolve) {
-			this._connectResolve(null);
-			this._connectResolve = null;
-		}
-		this._rbf = null;
-		this._ready_fin = false;
-		this._waitSendBufferWhenConnecting = null;
-
 		if (this._pipeline) {
 			this._pipeline.fnHandleClose(this, err);
 		}
 	}
 
-	readBuffer(data, rinfo) {
-		this._heartbeatTimes = 0;
-		this._lastRecvMsec = Date.now();
-		this._rbf = Buffer.concat([this._rbf, data]);
-		do {
-			let decodeObj;
-			try {
-				decodeObj = this._pipeline.fnDecodeBuffer(this._rbf, rinfo);
-				if (!decodeObj) {
-					break;
-				}
-				if (decodeObj.totalLength === undefined || decodeObj.totalLength === null) {
-					this._onClose(new Error("NetChannelBase::readBuffer miss totalLength field"));
-					return;
-				}
-				if (decodeObj.totalLength < 0) {
-					this._onClose(new Error("NetChannelBase::readBuffer decode exception"));
-					return;
-				}
-			} catch (e) {
-				this._onClose(new Error("NetChannelBase::readBuffer decode exception"));
-				return;
-			}
-			this._rbf = this._rbf.subarray(decodeObj.totalLength);
-			try {
-				this._pipeline.fnHandleDecodeObj(this, decodeObj);
-			}
-			catch (e) { void e; }
-		} while (this._rbf.length > 0);
-	}
-
-	fin() {
-		if (this._io) {
-			this._pipeline.fnIoFin(this._io);
-		}
-	}
-
-	close() {
-		this._onClose();
-	}
-
-	_writeConnectingCacheData() {
-		let buff = this._waitSendBufferWhenConnecting;
-		if (!buff) {
-			return;
-		}
-		this._waitSendBufferWhenConnecting = null;
-		this._pipeline.fnIoWrite(this._io, buff);
-	}
-
-	write(buff) {
-		if (!this._io) {
-			return;
-		}
-		if (typeof buff === 'string') {
-			buff = Buffer.from(buff);
-		}
-		if (NetChannelBase.CONNECT_STATUS_DOING == this._connectStatus) {
-			let newBuf;
-			if (this._waitSendBufferWhenConnecting) {
-				newBuf = Buffer.concat([this._waitSendBufferWhenConnecting, buff]);
-			}
-			else {
-				newBuf = Buffer.concat([buff]);
-			}
-			this._waitSendBufferWhenConnecting = newBuf;
-			return;
-		}
-		this._pipeline.fnIoWrite(this._io, buff);
-	}
-
-	useStdNetEvent() {
-		let self = this;
-		if (NetChannelBase.SOCK_STREAM == this._socktype) {
-			this._io.setNoDelay();
-			this._io.on('data', (data) => {
-				self.readBuffer(data, null);
-			});
-		}
-		else if (NetChannelBase.SOCK_DGRAM == this._socktype) {
-			this._io.on('message', (data, rinfo) => {
-				self.readBuffer(data, rinfo);
-			});
-		}
-		this._io.on('error', (err) => {
-			self._onClose(err);
-		});
-		this._io.on('close', () => {
-			self._onClose();
-		});
-		this._pipeline.fnIoWrite = (io, data) => {
-			io.write(data);
-		};
-		this._pipeline.fnIoFin = (io) => {
-			io.end();
-		};
-		this._pipeline.fnIoDestroy = (io) => {
-			io.destroy();
-		};
-	}
-
-	tcpConnect(host, port) {
-		if (this.side != NetChannelBase.CLIENT_SIDE) {
-			return null;
-		}
-		if (this._socktype != NetChannelBase.SOCK_STREAM) {
-			return null;
-		}
-		if (NetChannelBase.CONNECT_STATUS_NEW != this._connectStatus) {
-			return this;
-		}
-		let self = this;
+	_prepareConnect(resolve, resolve_ret) {
 		this._connectStatus = NetChannelBase.CONNECT_STATUS_DOING;
-		this._pipeline.fnIoFin = (io) => {
-			if (self._waitSendBufferWhenConnecting && self._waitSendBufferWhenConnecting.length > 0) {
-				self._ready_fin = true;
-				return;
-			}
-			io.end();
-		};
-		this._pipeline.fnIoDestroy = (io) => {
-			io.destroy();
-		};
-		return new Promise((resolve) => {
-			self._connectResolve = resolve;
-			let conn_timeout_id = null;
-			if (self.connectTimeoutMsec > 0) {
-				conn_timeout_id = setTimeout(() => {
-					clearTimeout(conn_timeout_id);
-					self._connectResolve = null;
-					self._io.destroy();
-					self._io = null;
-					self._onClose(new Error("NetChannelBase connect timeout"));
-					resolve(null);
-				}, self.connectTimeoutMsec);
-			}
-			self._io = net.createConnection({ host: host, port: port }, () => {
-				self._connectResolve = null;
-				self._connectStatus = NetChannelBase.CONNECT_STATUS_DONE;
-				if (conn_timeout_id) {
-					clearTimeout(conn_timeout_id);
-				}
-				self.useStdNetEvent();
-				self._writeConnectingCacheData();
-				if (self._ready_fin) {
-					self._io.end();
-					resolve(null);
-				}
-				else {
-					resolve(self);
-				}
-			});
-		});
+		this._connectResolve = resolve;
+		if (this.connectTimeoutMsec > 0) {
+			this._connectTimeout = setTimeout(() => {
+				this._connectResolve = null;
+				this.close(new Error("NetChannel connect timeout"));
+				resolve(resolve_ret);
+			}, this.connectTimeoutMsec);
+		}
+	}
+
+	_afterConnect() {
+		this._connectResolve = null;
+		this._connectStatus = NetChannelBase.CONNECT_STATUS_DONE;
+		if (this._connectTimeout) {
+			clearTimeout(this._connectTimeout);
+			this._connectTimeout = null;
+		}
 	}
 
 	setClientSideHeartbeat(fnHeartbeat, interval, maxTimes) {
@@ -270,13 +129,13 @@ class NetChannelBase {
 				return;
 			}
 			if (self._heartbeatTimes < maxTimes) {
-				++self._heartbeatTimes;
 				fnHeartbeat(self);
+				++self._heartbeatTimes;
 				self._heartbeatTimeout = setTimeout(fn, interval);
 				return;
 			}
 			self._heartbeatTimeout = null;
-			self._onClose(new Error("NetChannelBase client hearbeat timeout"));
+			self.close(new Error("NetChannelBase client hearbeat timeout"));
 		};
 		self._heartbeatTimeout = setTimeout(fn, interval);
 	}
@@ -300,99 +159,160 @@ class NetChannelBase {
 				return;
 			}
 			self._heartbeatTimeout = null;
-			self._onClose(new Error("NetChannelBase server hearbeat timeout"));
+			self.close(new Error("NetChannelBase server hearbeat timeout"));
 		}
 		self._heartbeatTimeout = setTimeout(fn, interval);
 	}
 }
 
-class NetBridgeClientHub extends NetChannelBase {
-	constructor(pipeline, io) {
-		super(pipeline, io, NetChannelBase.CLIENT_SIDE, 0);
-		this.publishChannel = new Map();
-		this.subscribeChannel = new Map();
+class NetChannel extends NetChannelBase {
+	NetChannel(pipeline, io, side, socktype) {
+		super(pipeline, io, side, socktype);
+		this._rbf = Buffer.alloc(0);
+		this._ready_fin = false;
+		this._waitSendBufferWhenConnecting = null;
+		this._initedEvent = false;
+		if (this._io) {
+			this.initEvent();
+			if (this.side == NetChannel.SERVER_SIDE) {
+				this._connectStatus = NetChannelBase.CONNECT_STATUS_DONE;
+			}
+		}
 	}
 
-	getChannel(side, key) {
-		if (NetChannelBase.PUBLISH_SIDE == side) {
-			return this.publishChannel.get(key);
-		}
-		else if (NetChannelBase.SUBSCRIBE_SIDE == side) {
-			return this.subscribeChannel.get(key);
-		}
-		return null;
-	}
-
-	addChannel(key, channel) {
-		if (NetChannelBase.PUBLISH_SIDE == channel._side) {
-			channel._connectStatus = this._connectStatus;
-			this.publishChannel.set(key, channel);
-		}
-		else if (NetChannelBase.SUBSCRIBE_SIDE == channel._side) {
-			this.subscribeChannel.set(key, channel);
-		}
-		else {
+	initEvent() {
+		if (this._initedEvent) {
 			return;
 		}
-		channel._io = this._io;
-		channel._pipeline.fnIoDestroy = (io) => { void io; };
-        channel._pipeline.fnIoFin = (io) => { void io; }
-	}
-
-	_onConnectSuccess() {
-		this._connectResolve = null;
-		this._connectStatus = NetChannelBase.CONNECT_STATUS_DONE;
-		for (let pub_ch of this.publishChannel.values()) {
-			pub_ch._connectStatus = NetChannelBase.CONNECT_STATUS_DONE;
-			pub_ch._writeConnectingCacheData();
+		this._initedEvent = true;
+		let self = this;
+		if (NetChannelBase.SOCK_STREAM == this._socktype) {
+			this._io.setNoDelay();
+			this._io.on('data', (data) => {
+				self.readBuffer(data, null);
+			});
 		}
+		else if (NetChannelBase.SOCK_DGRAM == this._socktype) {
+			this._io.on('message', (data, rinfo) => {
+				self.readBuffer(data, rinfo);
+			});
+		}
+		this._io.on('error', (err) => {
+			self.close(err);
+		});
+		this._io.on('close', () => {
+			self.close();
+		});
 	}
 
-	_onClose(err) {
-		for (const channel of this.publishChannel) {
+	close(err) {
+		this._rbf = null;
+		this._ready_fin = false;
+		this._waitSendBufferWhenConnecting = null;
+		if (this._io) {
+			this._io.destroy();
+		}
+		super.close(err);
+	}
+
+	readBuffer(data, rinfo) {
+		this._heartbeatTimes = 0;
+		this._lastRecvMsec = Date.now();
+		this._rbf = Buffer.concat([this._rbf, data]);
+		do {
+			let decodeObj;
 			try {
-				channel._onClose(err);
-			} catch (e) { void e; }
-		}
-		for (const channel of this.subscribeChannel) {
+				decodeObj = this._pipeline.fnDecodeBuffer(this._rbf, rinfo);
+				if (!decodeObj) {
+					break;
+				}
+				if (decodeObj.totalLength === undefined || decodeObj.totalLength === null) {
+					this.close(new Error("NetChannelBase::readBuffer miss totalLength field"));
+					return;
+				}
+				if (decodeObj.totalLength < 0) {
+					this.close(new Error("NetChannelBase::readBuffer decode exception"));
+					return;
+				}
+			} catch (e) {
+				this.close(new Error("NetChannelBase::readBuffer decode exception"));
+				return;
+			}
+			this._rbf = this._rbf.subarray(decodeObj.totalLength);
 			try {
-				channel._onClose(err);
-			} catch (e) { void e; }
-		}
-		try {
-			super._onClose(err);
-		} catch (e) { void e; }
+				this._pipeline.fnHandleDecodeObj(this, decodeObj);
+			}
+			catch (e) { void e; }
+		} while (this._rbf.length > 0);
 	}
 
-	setClientSideHeartbeat(fnHeartbeat, interval, maxTimes) {
-		if (NetChannelBase.CONNECT_STATUS_NEW == this._connectStatus) {
+	send(buff) {
+		if (!this._io) {
 			return;
+		}
+		if (typeof buff === 'string') {
+			buff = Buffer.from(buff);
+		}
+		if (NetChannelBase.CONNECT_STATUS_DOING == this._connectStatus) {
+			let newBuf;
+			if (this._waitSendBufferWhenConnecting) {
+				newBuf = Buffer.concat([this._waitSendBufferWhenConnecting, buff]);
+			}
+			else {
+				newBuf = Buffer.concat([buff]);
+			}
+			this._waitSendBufferWhenConnecting = newBuf;
+			return;
+		}
+		this._io.write(buff);
+	}
+
+	fin() {
+		if (this._waitSendBufferWhenConnecting) {
+			this._ready_fin = true;
+			return;
+		}
+		if (this._io) {
+			this._io.end();
+		}
+	}
+
+	tcpConnect(host, port) {
+		if (this.side != NetChannelBase.CLIENT_SIDE) {
+			return false;
+		}
+		if (this._socktype != NetChannelBase.SOCK_STREAM) {
+			return false;
+		}
+		if (NetChannelBase.CONNECT_STATUS_DONE == this._connectStatus) {
+			return true;
+		}
+		if (NetChannelBase.CONNECT_STATUS_NEW != this._connectStatus) {
+			return false;
 		}
 		let self = this;
-		if (self._heartbeatTimeout) {
-			clearTimeout(self._heartbeatTimeout);
-		}
-		const fn = function () {
-			clearTimeout(self._heartbeatTimeout);
-			if (NetChannelBase.CONNECT_STATUS_DONE != self._connectStatus) {
-				self._heartbeatTimeout = setTimeout(fn, interval);
-				return;
-			}
-			if (self._heartbeatTimes < maxTimes) {
-				++self._heartbeatTimes;
-				fnHeartbeat(self);
-				self._heartbeatTimeout = setTimeout(fn, interval);
-				return;
-			}
-			self._heartbeatTimeout = null;
-			self._onClose(new Error("TaomeeBridgeChannel hearbeat timeout"));
-		};
-		self._heartbeatTimeout = setTimeout(fn, interval);
+		return new Promise((resolve) => {
+			self._prepareConnect(resolve, false);
+			self._io = net.createConnection({ host: host, port: port }, () => {
+				self._afterConnect();
+				if (this._waitSendBufferWhenConnecting) {
+					this._io.write(this._waitSendBufferWhenConnecting);
+					this._waitSendBufferWhenConnecting = null;
+				}
+				if (self._ready_fin) {
+					self._io.end();
+					resolve(false);
+				}
+				else {
+					resolve(true);
+				}
+			});
+			self.initEvent();
+		});
 	}
 }
 
 module.exports = {
 	NetChannelPipelineBase,
-	NetChannelBase,
-	NetBridgeClientHub
+	NetChannelBase
 };
---
+++ nodejs/net_channel_ioredis.js
@@ -0,0 +1,71 @@
+const { NetChannelBase, NetChannelPipelineBase } = require('./net_channel_base.js');
+const Redis = require("ioredis");
+
+class NetIoRedisClientChannel extends NetChannelBase {
+    constructor(io) {
+        super(new NetChannelPipelineBase(), io, NetChannelBase.CLIENT_SIDE, NetChannelBase.SOCK_STREAM);
+        this._enableSubscribeEvent = false;
+        if (io) {
+            this._connectStatus = NetChannelBase.CONNECT_STATUS_DONE;
+            let self = this;
+            this._io.on('error', (err) => {
+                self.close(err);
+            });
+            this._io.on('close', () => {
+                self.close();
+            });
+        }
+    }
+
+    close(err) {
+        if (this._io) {
+            this._io.quit();
+        }
+        super.close(err);
+    }
+
+    connect(args) {
+        if (NetChannelBase.CONNECT_STATUS_DONE == this._connectStatus) {
+            return true;
+        }
+        if (NetChannelBase.CONNECT_STATUS_NEW != this._connectStatus) {
+            return false;
+        }
+        let self = this;
+        return new Promise((resolve) => {
+            self._prepareConnect(resolve, false);
+            self._io = new Redis(args);
+            self._io.on('ready', () => {
+                self._afterConnect();
+                resolve(true);
+            });
+            self._io.on('error', (err) => {
+                self.close(err);
+            });
+            self._io.on('close', () => {
+                self.close();
+            });
+        });
+    }
+
+    setHeartbeat(interval, maxTimes) {
+        super.setClientSideHeartbeat((channel) => {
+            channel._heartbeatTimes = 0;
+            channel._io.ping();
+        }, interval, maxTimes);
+    }
+
+    enableSubscribe(fnOnSubscribe) {
+        if (this._enableSubscribeEvent) {
+            return;
+        }
+        this._enableSubscribeEvent = true;
+        this._io.on('messageBuffer', (bufferSubscribeKey, data) => {
+            fnOnSubscribe(bufferSubscribeKey.toString(), data);
+        });
+    }
+}
+
+module.exports = {
+    NetIoRedisClientChannel
+};

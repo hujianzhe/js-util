5895f9a86454cd1d65447b9543901495e513f7bd
zzz <zzz@taomee-ex.com>
2024-10-18 12:35:32 +0800

    ...

--- nodejs/net_channel_ioredis.js
+++ nodejs/net_channel_ioredis.js
@@ -34,9 +34,7 @@ class NetIoRedisClientChannel extends NetChannelBase {
                 resolve(true);
             });
             self._io.on('error', (err) => {
-                if (self._connectStatus != NetChannelBase.CONNECT_STATUS_DONE) {
-                    self.close(err);
-                }
+                self.close(err);
             });
             self._io.on('close', () => {
                 self.close();

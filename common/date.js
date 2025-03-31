83adf7df0999ed3d0467eb268036109fdfec2b13
zzz <zzz@taomee-ex.com>
2025-03-31 13:54:37 +0800

    date

--- common/date.js
+++ common/date.js
@@ -15,9 +15,10 @@ js_util.Common.local_second = function () {
     return js_util.Common.gmt_second() - js_util.Common.time_zone_offset_second();
 };
 
-js_util.Common.gmt_second_diff_day = function (gmt_sec1, gmt_sec2) {
+js_util.Common.gmt_second_diff_day = function (gmt_sec1, gmt_sec2, offset_sec = 0) {
+    offset_sec = Math.abs(offset_sec);
     const tz_sec = js_util.Common.time_zone_offset_second();
-    const local_day1 = Math.floor((gmt_sec1 - tz_sec) / 86400);
-    const local_day2 = Math.floor((gmt_sec2 - tz_sec) / 86400);
-    return local_day1 - local_day2;
+    const day1 = Math.floor((gmt_sec1 - tz_sec - offset_sec) / 86400);
+    const day2 = Math.floor((gmt_sec2 - tz_sec - offset_sec) / 86400);
+    return day1 - day2;
 };
